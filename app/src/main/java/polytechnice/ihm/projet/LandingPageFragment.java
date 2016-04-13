51b0786452331f813ad2c8daf9e7edcb6bcbfc41
Anthony LOROSCIO <anthonyloroscio@gmail.com>
2016-04-13 20:00:09 +0200

    The landing page now have brief description of the application.

--- app/src/main/java/polytechnice/ihm/projet/LandingPageFragment.java
+++ app/src/main/java/polytechnice/ihm/projet/LandingPageFragment.java
@@ -1,6 +1,8 @@
 package polytechnice.ihm.projet;
 
+import android.graphics.Color;
 import android.support.annotation.DrawableRes;
+import android.support.annotation.StringDef;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -8,6 +10,7 @@ import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 import android.widget.TextView;
 
 import com.twitter.sdk.android.tweetui.SearchTimeline;
@@ -18,6 +21,9 @@ import com.twitter.sdk.android.tweetui.UserTimeline;
  * Created by anthony LOROSCIO on 04/04/2016.
  */
 public class LandingPageFragment extends ListFragment {
+
+    private String htmlText = "<html><body style=\"text-align:justify\"> <style type=\"text/css\">body{color: #fff;} </style> %s </body></Html>";
+    private String appIntroduction = "Bienvenue sur l'application Poly'News. Celle-ci vous permets de consulter l'actualité de l'école grace aux articles écrits par nos meilleurs rédacteurs, ou bien grace au flux twitter de l'école.";
 
     public void onCreate(Bundle savedInstanceState){
 
@@ -35,7 +41,13 @@ public class LandingPageFragment extends ListFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.landing_page, container, false);
+        View v = inflater.inflate(R.layout.landing_page, container, false);
+
+        WebView webView = (WebView) v.findViewById(R.id.presentationText);
+        webView.loadDataWithBaseURL(null, String.format(htmlText, appIntroduction), "text/html", "utf-8", null);
+        webView.setBackgroundColor(Color.TRANSPARENT);
+
+        return v;
 
     }
 
