b6788b6ed927d32f86f94a61251a99d89171ee90
Patrice Camousseigt <camoussdu83@hotmail.fr>
2016-04-15 16:32:33 +0200

    Correction of the presentation text.

--- app/src/main/java/polytechnice/ihm/projet/LandingPageFragment.java
+++ app/src/main/java/polytechnice/ihm/projet/LandingPageFragment.java
@@ -23,7 +23,7 @@ import com.twitter.sdk.android.tweetui.UserTimeline;
 public class LandingPageFragment extends ListFragment {
 
     private String htmlText = "<html><body> <style type=\"text/css\">body{color: #fff; font-size: 0.9em; text-align:justify;} </style> %s </body></Html>";
-    private String appIntroduction = "Bienvenue sur l'application Poly'News. Celle-ci vous permets de consulter l'actualité de l'école grace aux articles ou bien grace au flux twitter de l'école.";
+    private String appIntroduction = "Bienvenue sur l'application Poly'News. Celle-ci vous permet de consulter l'actualité de l'école grâce aux articles et au flux twitter de l'école.";
 
     public void onCreate(Bundle savedInstanceState){
 
