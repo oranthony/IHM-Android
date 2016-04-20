f02241840ce4bfc1fd7fee9183c0eb01e23635f8
Anthony LOROSCIO <anthonyloroscio@gmail.com>
2016-04-20 13:56:05 +0200

    The sliding tweet view on the landing page do no scroll anymore when collapsed.

--- app/src/main/java/polytechnice/ihm/projet/LandingPageFragment.java
+++ app/src/main/java/polytechnice/ihm/projet/LandingPageFragment.java
@@ -50,34 +50,10 @@ public class LandingPageFragment extends ListFragment {
 
         _menuListView = (ListView) v.findViewById(android.R.id.list);
 
-        /*_menuListView.setOnTouchListener(new View.OnTouchListener() {
-
+        _menuListView.setOnTouchListener( new View.OnTouchListener(){
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                int action = event.getAction();
-                switch (action) {
-                    case MotionEvent.ACTION_DOWN:
-                        // Disallow ScrollView to intercept touch events.
-                        v.getParent().requestDisallowInterceptTouchEvent(true);
-                        break;
-
-                    case MotionEvent.ACTION_UP:
-                        // Allow ScrollView to intercept touch events.
-                        v.getParent().requestDisallowInterceptTouchEvent(false);
-                        break;
-                }
-
-                // Handle ListView touch events.
-                v.onTouchEvent(event);
                 return true;
-            }
-        });*/
-
-
-        _menuListView.setOnTouchListener(new View.OnTouchListener() {
-
-            public boolean onTouch(View v, MotionEvent event) {
-                return (event.getAction() == MotionEvent.ACTION_MOVE);
             }
         });
 
