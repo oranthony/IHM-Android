dc26be8eda3a260d19af2a232feb3df0b6b34af2
Anthony LOROSCIO <anthonyloroscio@gmail.com>
2016-04-29 11:03:44 +0200

    Add style improvements and Polytech icon.

--- app/src/main/java/polytechnice/ihm/projet/TwitterFragment.java
+++ app/src/main/java/polytechnice/ihm/projet/TwitterFragment.java
@@ -1,27 +1,20 @@
 package polytechnice.ihm.projet;
 
-import android.app.Fragment;
-import android.support.v4.app.ListFragment;
 import android.os.Bundle;
+import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.digits.sdk.android.Digits;
-import com.twitter.sdk.android.Twitter;
-import com.twitter.sdk.android.core.TwitterAuthConfig;
-import com.twitter.sdk.android.core.TwitterCore;
-import com.twitter.sdk.android.core.TwitterSession;
 import com.twitter.sdk.android.tweetui.SearchTimeline;
 import com.twitter.sdk.android.tweetui.TweetTimelineListAdapter;
-import com.twitter.sdk.android.tweetui.UserTimeline;
-
-import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by anthony LOROSCIO on 28/03/2016.
  */
 public class TwitterFragment extends ListFragment{
+
+    //TODO Rename view like #Polytech'Nice => understand that the 2 views are different => Mur de tweet
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
